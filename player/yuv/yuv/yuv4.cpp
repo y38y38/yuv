720974d0258abda9ee01aca4209c77f364dcb0d9
Yuusuke Miyazaki <y38y38@gmail.com>
2017-05-13 22:55:51 +0900

    add diff

--- player/yuv/yuv/yuv4.cpp
+++ player/yuv/yuv/yuv4.cpp
@@ -86,8 +86,6 @@ void Yuv4::getPixel(unsigned char *buffer, int pixel, short *y, short *pb, short
 	*y = *(buffer + height_offset + width + y_offset_cbcr + y_offset_y);
 
 
-	//*pb = *(buffer + (max_width * max_height) + ((width / 2) + ((height / 2) * (max_width / 2))));
-	//*pr = *(buffer + (max_width * max_height) + ((max_width / 2) * (max_height / 2)) + ((width / 2) + ((height / 2) * (max_width / 2))));
 	int cbcr_offset;
 	if (pixel == 0) {
 		cbcr_offset = 0;
@@ -123,5 +121,127 @@ void Yuv4::getPixel(unsigned char *buffer, int pixel, short *y, short *pb, short
 
 	pr_value = *(buffer + height_offset + cb_offset_y + cb_offset_cr + (width / 2) + 1);
 	*pr = (pr_value + 128) & 0xff;
+	return;
+}
+void Yuv4::getYPixel(unsigned char *buffer, int pixel, short *y, short *pb, short *pr, int max_width, int max_height)
+{
+	int height;
+	int width = pixel % max_width;
+	if (pixel == 0) {
+		height = 0;
+	}
+	else {
+		height = pixel / max_width;
+	}
+
+
+
+	int y_offset_cbcr = 0;
+	if (width == 0) {
+		y_offset_cbcr = 2;
+	}
+	else {
+		y_offset_cbcr = (((width / 2) * 2) + 2);
+	}
+
+	int y_offset_y = 0;
+	if ((height % 2) == 0) {
+		y_offset_y = (((width / 2) * 2));
+	}
+	else {
+		y_offset_y = (((width / 2) * 2) + 2);
+	}
+	int height_offset;
+
+	if (height == 0) {
+		height_offset = 0;
+	}
+	else {
+		height_offset = (height / 2) * (max_width * 3);
+
+	}
+
+
+	*y = *(buffer + height_offset + width + y_offset_cbcr + y_offset_y);
+
+
+	int cbcr_offset;
+	if (pixel == 0) {
+		cbcr_offset = 0;
+	}
+	else {
+		cbcr_offset = ((pixel) / 4);
+	}
+
+	int cb_offset_y;
+	if (width == 0) {
+		cb_offset_y = 0;
+	}
+	else {
+		cb_offset_y = (((width / 2) * 2)) * 2;
+	}
+
+	int cb_offset_cr;
+	if (width == 0) {
+		cb_offset_cr = 0;
+	}
+	else {
+		cb_offset_cr = width / 2;
+	}
+
+
+
+	if (width == 0) {
+		width = 1;
+	}
+	short pb_value, pr_value;
+	pb_value = *(buffer + height_offset + cb_offset_y + cb_offset_cr + (width / 2));
+	*pb = (pb_value + 128) & 0xff;
+
+	pr_value = *(buffer + height_offset + cb_offset_y + cb_offset_cr + (width / 2) + 1);
+	*pr = (pr_value + 128) & 0xff;
+
+}
+void Yuv4::setYPixel(unsigned char y, int pixel, unsigned char *buf, int max_width, int max_heightf)
+{
+	int height;
+	int width = pixel % max_width;
+	if (pixel == 0) {
+		height = 0;
+	}
+	else {
+		height = pixel / max_width;
+	}
+
+
+	int y_offset_cbcr = 0;
+	if (width == 0) {
+		y_offset_cbcr = 2;
+	}
+	else {
+		y_offset_cbcr = (((width / 2) * 2) + 2);
+	}
+
+	int y_offset_y = 0;
+	if ((height % 2) == 0) {
+		y_offset_y = (((width / 2) * 2));
+	}
+	else {
+		y_offset_y = (((width / 2) * 2) + 2);
+	}
+	int height_offset;
+
+	if (height == 0) {
+		height_offset = 0;
+	}
+	else {
+		height_offset = (height / 2) * (max_width * 3);
+
+	}
+
+
+	*(buf + height_offset + width + y_offset_cbcr + y_offset_y) = y;
+
+
 	return;
 }
--- player/yuv/yuv/yuv_diff.cpp
+++ player/yuv/yuv/yuv_diff.cpp
@@ -26,19 +26,29 @@ YuvDiff::~YuvDiff(void)
 
 void YuvDiff::CreateDiff(uint8_t *img1, uint8_t *img2)
 {
-	uint8_t diff;
+	uint32_t width = YuvSetting::GetInst().GetWidthSize();
+	uint32_t height = YuvSetting::GetInst().GetHeightSize();
+
+	int8_t diff;
 	for (uint32_t i = 0; i < Width; i++) {
 		for (uint32_t j = 0; j < Height; j++) {
 			short y1, y2, pb, pr;
-			RawVideo420::getYPixel(img1, (i % Width) + (j * Width), &y1, &pb, &pr);
-			RawVideo420::getYPixel(img2, (i % Width) + (j * Width), &y2, &pb, &pr);
-			if (y1 > y2) {
-				diff = y1 - y2;
+			if (YuvSetting::GetInst().GetFormat() == YuvSetting::YUV_FORMAT_YV12) {
+				RawVideo420::getYPixel(img1, (i % Width) + (j * Width), &y1, &pb, &pr);
+				RawVideo420::getYPixel(img2, (i % Width) + (j * Width), &y2, &pb, &pr);
 			}
 			else {
-				diff = y2 - y1;
+				Yuv4::getYPixel(img1, (i % Width) + (j * Width), &y1, &pb, &pr, width, height);
+				Yuv4::getYPixel(img2, (i % Width) + (j * Width), &y2, &pb, &pr, width, height);
+
 			}
-			RawVideo420::setYPixel(diff, (i % Width) + (j * Width), YuvBuffer);
+			diff = 0x80 + (y1 - y2);
+			if (YuvSetting::GetInst().GetFormat() == YuvSetting::YUV_FORMAT_YV12) {
+				RawVideo420::setYPixel(diff, (i % Width) + (j * Width), YuvBuffer);
+			}
+			else {
+				Yuv4::setYPixel((unsigned char )diff, (i % Width) + (j * Width), YuvBuffer, width, height);
+			}
 		}
 	}
 
@@ -46,7 +56,13 @@ void YuvDiff::CreateDiff(uint8_t *img1, uint8_t *img2)
 }
 void YuvDiff::GetRgb(uint8_t *rgbbuf)
 {
-	RawVideo420::getRgb(YuvBuffer, Width, Height, rgbbuf, TRUE, TRUE, TRUE);
+	if (YuvSetting::GetInst().GetFormat() == YuvSetting::YUV_FORMAT_YV12) {
+		RawVideo420::getRgb(YuvBuffer, Width, Height, rgbbuf, TRUE, TRUE, TRUE);
+	}
+	else {
+		Yuv4::getRgb(YuvBuffer, Width, Height, rgbbuf, TRUE, TRUE, TRUE);
+
+	}
 	return;
 }
 
@@ -67,6 +83,10 @@ void YuvDiff::SetSize(uint32_t width, uint32_t height)
 	YuvBuffer = (uint8_t *)malloc(Width * Height + ((Width * height) / 2));
 	if (YuvBuffer == NULL) {
 		Win32Printf("%hs %d malloc error", __func__, __LINE__);
+	}
+
+	if (YuvSetting::GetInst().GetFormat() == YuvSetting::YUV_FORMAT_YUV4) {
+		memset(YuvBuffer, 0x00, Width * Height + ((Width * height) / 2));
 	}
 
 	return;
