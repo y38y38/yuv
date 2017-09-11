18e977a0bfe92b328df7b76cce61cd85765988f7
Yuusuke Miyazaki <y38y38@gmail.com>
2017-09-11 19:27:57 +0900

    delete file

--- audio/lib_enc_g711/enc_g711.h
+++
@@ -1,8 +0,0 @@
-#ifndef _ENCG711_
-#define _ENCG711_
-
-
-void EncG711Send(unsigned long id, void* address, unsigned long size);
-typedef void (* FUNC_POINTER)(unsigned long id);
-void EncG711RegisterFunc(FUNC_POINTER func_ptr);
-#endif /* _ENCG711_ */
