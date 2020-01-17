b83de93525253736ec1970fb5a46c697ec23ff59
Yuusuke <y38y38@gmail.com>
2020-01-18 01:12:55 +0900

    ok

--- formatconv/422_10_to_420_8/chg.sh
+++ formatconv/422_10_to_420_8/chg.sh
@@ -1,6 +1,6 @@
-../../../sample/yuv10to8 $1 tmp.yuv $3 $4
-../../../sample/yuv422to420 tmp.yuv $2 $3 $4
-cp $2 /mnt/c/work/yuv_${3}x${4}.yuv
+/home/y38y38/yuv/formatconv/422_10_to_420_8/yuv10to8 $1 tmp.yuv $3 $4
+/home/y38y38/yuv/formatconv/422_10_to_420_8/yuv422to420 tmp.yuv $2 $3 $4
+cp $2 /mnt/c/work/${1%.yuv}_${3}x${4}.yuv
 
 
 
