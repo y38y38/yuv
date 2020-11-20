7d81634ccc1a0c8d3ad0a8d18fbf81651bd42241
Yuusuke <y38y38@gmail.com>
2020-11-21 01:36:38 +0900

    del log

--- formatconv/422_10_to_420_8/yuv10422_1088to1080.c
+++ formatconv/422_10_to_420_8/yuv10422_1088to1080.c
@@ -34,7 +34,9 @@ int main(int argc, char **argv)
     for (int i=0;;i++) {
         size_t readsize = fread(src_data, 1, src_size, input);
         if (readsize != src_size) {
-            printf("%d %d\n", __LINE__, (int)readsize);
+            if (readsize != 0 ) {
+                printf("%d %d\n", __LINE__, (int)readsize);
+            }
             break;
         }
         //y
@@ -56,7 +58,7 @@ int main(int argc, char **argv)
           //break;
         }
         //printf("end frame\n");
-        printf(".");
+        //printf(".");
     }
 
     return 0;
