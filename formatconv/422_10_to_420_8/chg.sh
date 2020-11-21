420b9cdcbf12ab6d807a93ae5ddb2d5c53152f66
Yuusuke <y38y38@gmail.com>
2020-11-21 16:03:57 +0900

    add tmp

--- formatconv/422_10_to_420_8/chg.sh
+++ formatconv/422_10_to_420_8/chg.sh
@@ -1,5 +1,6 @@
-/home/y/prores/yuv/formatconv/422_10_to_420_8/yuv10to8 $1 tmp.yuv $3 $4
-/home/y/prores/yuv/formatconv/422_10_to_420_8/yuv422to420 tmp.yuv $2 $3 $4
+mkdir -p ./tmp/
+/home/y/prores/yuv/formatconv/422_10_to_420_8/yuv10to8 $1 ./tmp/tmp.yuv $3 $4
+/home/y/prores/yuv/formatconv/422_10_to_420_8/yuv422to420 ./tmp/tmp.yuv $2 $3 $4
 #cp $2 /mnt/c/work/${basename ${1}}_${3}x${4}.yuv
 
 
--- formatconv/444_10_to_420_8/chg.sh
+++ formatconv/444_10_to_420_8/chg.sh
@@ -1,5 +1,6 @@
-/home/y/prores/yuv/formatconv/444_10_to_420_8/yuv10to8 $1 tmp.yuv $3 $4
-/home/y/prores/yuv/formatconv/444_10_to_420_8/yuv444to420 tmp.yuv $2 $3 $4
+mkdir -p ./tmp/
+/home/y/prores/yuv/formatconv/444_10_to_420_8/yuv10to8 $1 ./tmp/tmp.yuv $3 $4
+/home/y/prores/yuv/formatconv/444_10_to_420_8/yuv444to420 ./tmp/tmp.yuv $2 $3 $4
 #cp $2 /mnt/c/work/${basename ${1}}_${3}x${4}.yuv
 
 
