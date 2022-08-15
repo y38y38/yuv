a40ca5cdd08a6a4ed3bb9e467e6aee5456791dd7
Yuusuke <y38y38@gmail.com>
2022-08-15 18:17:58 +0900

    debug

--- common/cmm.c
+++ common/cmm.c
@@ -52,3 +52,47 @@ void cmmGetCrPixel(unsigned short *buffer, int pixel, unsigned short *cr, int ma
 	*cr = tmp;
     //*cr = *(buffer + ((max_height * 2) * (max_width * 2)) + (max_height * max_width ) + ((height * max_width) + width));
 }
+
+void cmmSetYPixel(unsigned short *buffer, int pixel, unsigned short y, int max_width, int  max_height) {
+	//pointer is short
+    int width = pixel % max_width;
+    int height= 0;
+    if (pixel == 0) {
+        height = 0;
+    } else {
+        height = pixel / max_width;
+    }
+
+    *(buffer + (( height * max_width) + width)) = y;
+    return;
+}
+
+void cmmSetCbPixel(unsigned short *buffer, int pixel, unsigned short cb, int max_width, int  max_height) {
+	//pointer is short
+	
+    int width = pixel % max_width;
+    int height= 0;
+    if (pixel == 0) {
+        height = 0;
+    } else {
+        height = pixel / max_width;
+    }
+
+    *(buffer + ((max_height * 2) * (max_width * 2)) + ((height * (max_width)) + width)) = cb;
+}
+
+void cmmSetCrPixel(unsigned short *buffer, int pixel, unsigned short cr, int max_width, int  max_height) {
+	//pointer is short
+    int width = pixel % max_width;
+    int height= 0;
+    if (pixel == 0) {
+        height = 0;
+    } else {
+        height = pixel / max_width;
+    }
+	int offset =  ((max_height * 2) * (max_width * 2)) + (max_height * max_width ) + ((height * max_width) + width);
+    *(buffer + offset) = cr;
+//    unsigned short tmp  = *(buffer + offset);
+//	tmp = cr;
+    //*cr = *(buffer + ((max_height * 2) * (max_width * 2)) + (max_height * max_width ) + ((height * max_width) + width));
+}
--- common/cmm.h
+++ common/cmm.h
@@ -9,4 +9,11 @@ void cmmGetCbPixel(unsigned short *buffer, int pixel, unsigned short *cb, int ma
 
 void cmmGetCrPixel(unsigned short *buffer, int pixel, unsigned short *cr, int max_width, int  max_height);
 
+
+void cmmSetYPixel(unsigned short *buffer, int pixel, unsigned short y, int max_width, int  max_height); 
+
+void cmmSetCbPixel(unsigned short *buffer, int pixel, unsigned short cb, int max_width, int  max_height);
+
+void cmmSetCrPixel(unsigned short *buffer, int pixel, unsigned short cr, int max_width, int  max_height);
+
 #endif //_CMM_H_
